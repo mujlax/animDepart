2aa68bf6e4a0aacabdbfc69b19216e0480505f30
deonease <zx053a@yandex.ru>
2024-10-23 23:30:05 +0300

    inite

---
+++ index.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+const { exec } = require('child_process');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadFile('index.html');
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// Обработка вызова AppleScript
+ipcMain.on('run-applescript', (event, script) => {
+    exec(`osascript -e '${script}'`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Ошибка: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`Стандартная ошибка: ${stderr}`);
+            return;
+        }
+        console.log(`Результат: ${stdout}`);
+        event.reply('applescript-response', stdout);
+    });
+});
