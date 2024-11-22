7f79df4357caefa92e2ada903df5bfbfbd4c3c04
deonease <zx053a@yandex.ru>
2024-11-22 14:34:32 +0300

    fix Globalcontext and repair callbacks

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
             contextIsolation: true,
+            enableRemoteModule: true
         },
     });
 
--- preload.js
+++ preload.js
@@ -3,5 +3,7 @@ const { contextBridge, webUtils, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
     getFilePath: (file) => webUtils.getPathForFile(file), // Безопасно возвращает путь
     processFolders: (data) => ipcRenderer.send('process-folders', data),
-    onProcessFoldersResponse: (callback) => ipcRenderer.on('process-folders-response', (event, response) => callback(response))
+    onProcessFoldersResponse: (callback) => ipcRenderer.on('process-folders-response', (event, response) => callback(response)),
+    send: (channel, data) => ipcRenderer.send(channel, data),
+    on: (channel, callback) => ipcRenderer.on(channel, (event, ...args) => callback(...args)),
 });
